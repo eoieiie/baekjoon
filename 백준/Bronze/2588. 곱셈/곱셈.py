ab821851294d058829bcbb2858b6b22a64b3206f
eoieiie <132263583+eoieiie@users.noreply.github.com>
2024-03-09 22:55:10 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,13 @@
+one = input()
+two = input()
+
+a = int(two[2]) * int(one)
+b = int(two[1]) * int(one)
+c = int(two[0]) * int(one)
+
+d = a + b*10 + c *100
+
+print(a)
+print(b)
+print(c)
+print(d)
