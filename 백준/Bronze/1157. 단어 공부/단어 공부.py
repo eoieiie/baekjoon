01bcef06ab4f436b7c8d3f4386607322f18c77d1
eoieiie <132263583+eoieiie@users.noreply.github.com>
2024-03-12 22:20:47 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -3,12 +3,11 @@ word_list = list(set(word))
 
 cnt = []
 for i in word_list:
-  count = word.count
-  cnt.append(count(i))
+  count = word.count(i)
+  cnt.append(count)
 
 if cnt.count(max(cnt)) > 1:
   print("?")
 
 else:
   print(word_list[(cnt.index(max(cnt)))])
-
