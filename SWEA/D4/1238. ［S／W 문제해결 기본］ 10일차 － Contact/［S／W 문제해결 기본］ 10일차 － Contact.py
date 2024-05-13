d5c555fcb124cd78ad3cf9b38b53e7306d521f37
eoieiie <132263583+eoieiie@users.noreply.github.com>
2024-05-13 22:13:19 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 109 ms, Memory: 48,100 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
@@ -1,36 +1,28 @@
-from collections import deque
-
-def solution(graph, start):
-    visited = list() #방문한 노드를 저장하는 리스트 
-    queue = deque() # bfs를 위한 큐
-    
-    queue.append((start,0))
-    visited.append(start)
-    howlen=list() #각 노드까지의 거리를 저장하는 리스트
-    while queue:
-        node, count = queue.popleft()
-        if node in graph:
-            for i in graph[node]:
-                if i not in visited:
-                    visited.append(i)
-                    howlen.append((i,count+1))
-                    queue.append((i,count+1))
-    howlen.sort(key = lambda x : (-x[1],-x[0]))
-    return howlen[0][0]
-
-for test_case in range(1,11): #10번 반복할건데 
-    N, start = map(int,input().split()) #일단 노드의 개수와 시작 노드를 입력받는다. 
-    numlist = list(map(int,input().split())) #간선 정보를 입력받는다. 
-    graph={} #그래프를 나타내는 딕셔너리. 예) {1: [2, 3], 2: [1, 4], ...}
-    for i in range(0,N,2): #입력된 간선 정보를 그래프로 변환하는 작업...
-        if numlist[i] not in graph:
-            graph[numlist[i]] = [numlist[i+1]]
-        else:
-            graph[numlist[i]].append(numlist[i+1])
-
-    print('#{} {}'.format(test_case, solution(graph,start)))
+def getsome(here,hour):
+    for next in mymap[here]:
+        if distance[next]>=hour + 1:
+            distance[next] = hour + 1
+            getsome(next, hour + 1)
 
 
+for tc in range(1, 11):
+    ans = 0
+    max_distance = 0
+    howmany, start = map(int, input().split())
+    data = list(map(int, input().split()))
 
-#쉽게 말하자면 주어진 그래프와 시작 노드를 받아서 함수를 수행하고, 가장 멀리 떨어진 노드를 찾아서 반환하는 문제.
-# 여기서 dfs인지 bfs인지 판별하는 방법은, 큐를 사용하냐 안 하냐, 팝을 이용하요 큐에서 값을 빼고 넣느냐의 관점엣서 보면 된다. 
+    distance = [9999] * 101
+    mymap = [[]for _ in range(101)]
+
+    for i in range(0, len(data), 2):
+        _from, _to = data[i], data[i + 1]
+        mymap[_from].append(_to)  
+
+    getsome(start, 0)
+
+    for i in range(1, len(distance)):
+        if distance[i] >=max_distance and distance[i] != 9999:
+            max_distance = distance[i]
+            ans = i
+
+    print("#%d %d" %(tc,ans))
